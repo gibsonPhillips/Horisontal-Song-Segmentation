6973e0c968bc3abda6a3b89c85fb20ceeb654a3d
Seth Frank <sbfrank@wpi.edu>
2024-11-12 08:45:47 -0500

    Adding looping in

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,5 @@
 import csv
-import statistics
+import os
 
 # import numpy as np
 
@@ -7,19 +7,8 @@ from Annotated_Intake import *
 from Data_Intake import *
 
 ### config data
-ACCEPTABLE_THRESHOLD = 3
-
-
-
-noID_segment_boundaries = [
-    0.000000,
-    24.751687,
-    67.408604,
-    110.077125,
-    134.074542,
-    176.754667
-]
-
+ACCEPTABLE_THRESHOLD_1 = 0.5
+ACCEPTABLE_THRESHOLD_2 = 3
 
 # outputs tuples as timestamps (boundary, nearest beat, nearest downbeat), distance(seconds)
 def nearest_beat_finder(algo_boundary, anno_beats):
@@ -91,7 +80,7 @@ def AbVa(list):
 def boundariesGroundTruth(distToGTBounds, totalBounds):
     gtBoundsNum = 0
     for distGTBound in distToGTBounds:
-        if abs(distGTBound) < ACCEPTABLE_THRESHOLD:
+        if abs(distGTBound) < ACCEPTABLE_THRESHOLD_2:
             gtBoundsNum += 1
     return gtBoundsNum/totalBounds
 
@@ -100,7 +89,7 @@ def groundTruthBoundariesFound(distToGTBounds, nearestGTBounds, gtBounds):
     for i in range(len(nearestGTBounds)):
         gtBound = nearestGTBounds[i]
         distGTBound = distToGTBounds[i]
-        if abs(distGTBound) < ACCEPTABLE_THRESHOLD and gtBound not in gtBoundsFound:
+        if abs(distGTBound) < ACCEPTABLE_THRESHOLD_2 and gtBound not in gtBoundsFound:
             gtBoundsFound.add(gtBound)
     return len(gtBoundsFound)/len(gtBounds)
 
@@ -139,7 +128,7 @@ def make_csv_tuple(boundaries, nearest_beats, nearest_beats_distance, nearest_se
 
 
 # write distances to a CSV file
-def write_csv(anno_beats, anno_segments, boundaries, nearest_beats, nearest_beats_distance, nearest_segments, nearest_segments_distance):
+def write_csv(csv_name, song_name, anno_beats, anno_segments, boundaries, nearest_beats, nearest_beats_distance, nearest_segments, nearest_segments_distance):
     data = make_csv_tuple(boundaries,
                           nearest_beats,
                           nearest_beats_distance,
@@ -148,8 +137,11 @@ def write_csv(anno_beats, anno_segments, boundaries, nearest_beats, nearest_beat
     
     averageTimeBetweenBeats = float(anno_beats[1][0]) - float(anno_beats[0][0])
 
-    with open('segment_boundary_distances.csv', mode='w', newline='') as csv_file:
+    with open(csv_name, mode='a', newline='') as csv_file:
         csv_writer = csv.writer(csv_file)
+
+        csv_writer.writerow([])
+        csv_writer.writerow([song_name])
 
         # !!! update headers
         # headers
@@ -177,17 +169,17 @@ anno_beats = beat_intake(anno_beats_txt)
 anno_segments_txt = "Baseline/Segments/0094_fireflies.txt"
 anno_segments = segment_intake(anno_segments_txt)
 
-algo_segments_txt = "outputs/Librosa_KmeansCQT_Cluster4/Fireflies_Librosa_KMeansCQT_Cluster4.csv"
-algo_segments = T2__parse_algo_beats_txt_to_tuples(algo_segments_txt)
-
-## ((boundary, nearest beat timestamp, nearest downbeat timestamp)), ((boundary - nearest_beat, boundary - nearest_downbeat))
-nearest_beats, nearest_beats_distance = nearest_beat_finder(algo_segments, anno_beats)
-nearest_segments, nearest_segments_distance = nearest_segment_finder(algo_segments, anno_segments)
-## ((boundary, nearest_segment)), (boundary - nearest_segment)
-
-# raw = [algo_segments, nearest_beats, nearest_beats_distance, nearest_segments, nearest_segments_distance]
-
-write_csv(anno_beats, anno_segments, algo_segments, nearest_beats, nearest_beats_distance, nearest_segments, nearest_segments_distance)
+for folder in os.scandir("outputs"):
+    print(folder.name)
+    csv_name = folder.name + ".csv"
+    with open(csv_name, mode='w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow([folder.name])
+    for song in os.scandir(folder.path):
+        algo_segments = T2__parse_algo_beats_txt_to_tuples(song.path)
+        nearest_beats, nearest_beats_distance = nearest_beat_finder(algo_segments, anno_beats)
+        nearest_segments, nearest_segments_distance = nearest_segment_finder(algo_segments, anno_segments)
+        write_csv(csv_name, song.name, anno_beats, anno_segments, algo_segments, nearest_beats, nearest_beats_distance, nearest_segments, nearest_segments_distance)
 
 
 
